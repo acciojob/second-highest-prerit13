034336cdc1d8445fc48b0effebda540ebe2c100b
prerit13 <147128014+prerit13@users.noreply.github.com>
2025-04-08 23:49:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+<script>
+function secondHighest(arr) {
+    if (arr.length < 2) {
+        return -Infinity;
+    }
+
+    let highest = -Infinity;
+    let second_highest = -Infinity;
+
+    for (let num of arr) {
+        if (num > highest) {
+            second_highest = highest;
+            highest = num;
+        } else if (num > second_highest && num !== highest) {
+            second_highest = num;
+        }
+    }
+
+    return second_highest === -Infinity ? -Infinity : second_highest;
+}
+</script>
